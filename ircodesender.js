aa42d66d24339ce7f103e7c89fae06066ff5768c
andrew-datasift <andrew.murphy@datasift.com>
2016-10-25 20:27:07 +0100

    adding teleport command and correcting some IRCODE values

--- ircodesender.js
+++ ircodesender.js
@@ -3,3 +3,9 @@ function sendtotivo(IRCODE) {
     xmlHttp.open('GET', "ircodesender.php?IRCOMMAND=" + IRCODE, true);
     xmlHttp.send( null );
 }
+
+function sendteleport(COMMAND) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', "ircodesender.php?TELEPORT=" + COMMAND, true);
+    xmlHttp.send( null );
+}
--- ircodesender.php
+++ ircodesender.php
@@ -4,10 +4,27 @@ http_response_code(204);
 
 if (isset($_GET['IRCOMMAND'])){
 
+  # The remote commands are TCP messages in the form "IRCODE [message]"
+  # message is sent as uri attribute
+  $ircomm = "IRCODE " . $_GET['IRCOMMAND'] . "\r\n";
+  sendCommand($ircomm);
+  
+}
+
+if (isset($_GET['TELEPORT'])){
+
+  # Teleport commands are TCP messages in the form "TELEPORT [message]"
+  # message is sent as uri attribute
+  $telecomm = "TELEPORT " . $_GET['TELEPORT'] . "\r\n";
+  sendCommand($telecomm);
+
+}
+
+function sendCommand($command) {
   # The default tivo port is 31339, some may let you configure this
   $port = 31339;
   # The address will need to be changed to that of your tivo box
-  $address = '127.0.0.1';
+  $address = '192.168.0.6';
 
   # Create a TCP/IP socket.
   $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -21,15 +38,13 @@ if (isset($_GET['IRCOMMAND'])){
       echo "Connect failed with reason: " . socket_strerror(socket_last_error($socket)) . "\n";
   }
 
-  # The remote commands are TCP messages in the form "IRCODE [message]"
-  # message is sent as uri attribute
-  $in = "IRCODE " . $_GET['IRCOMMAND'] . "\r\n";
-
-  socket_write($socket, $in, strlen($in));
+  socket_write($socket, $command, strlen($command));
 
   # Write out the response. The JS remote does not use this, but useful for debug
   echo socket_read($socket, 2048);
 
   socket_close($socket);
+
 }
+
 ?>
